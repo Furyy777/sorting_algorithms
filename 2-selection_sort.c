db88853e90fd1ce8d8908c37ab5a43341195da0f
Anassselasri <anaselasri37@gmail.com>
2024-01-28 02:12:07 +0100

    sorting_al g orithms update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,35 +1,34 @@
 #include "sort.h"
 
 /**
-*selection_sort- function that sorts using selection sort algorithm
-*
-*@array: array to be sorted
-*@size: size of the array
-*
-*/
+ * selection_sort - function that sorts using selection sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ */
 void selection_sort(int *array, size_t size)
 {
-size_t i, j, k;
-int temp;
+	size_t i, j, k;
+	int temp;
 
-if (!array || !size)
-	return;
+	if (!array || !size)
+		return;
 
-for (i = 0; i < size - 1; i++)
-{
-	k = i;
-	for (j = i + 1; j < size; j++)
+	for (i = 0; i < size - 1; i++)
 	{
-	if (array[j] < array[k])
-	{
-		k = j;
+		k = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[k])
+			{
+				k = j;
+			}
+		}
+		if (k != i)
+		{
+			temp = array[i];
+			array[i] = array[k];
+			array[k] = temp;
+			print_array(array, size);
+		}
 	}
-	}
-	if (array[i] > array[k])
-	{
-	temp = array[i];
-	array[i] = array[k];
-	array[k] = temp;
-	}
-}
 }
